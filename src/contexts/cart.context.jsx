ad0066fac7649cbb1e78d1351adfc40fd6ebc03f
YunusEmreUslu <usluyunusemre00@gmail.com>
2022-08-21 00:42:37 +0300

    finished checkout-item.component.jsx (124)

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -37,12 +37,16 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -64,12 +68,16 @@ export const CartProvider = ({ children }) => {
       setCartItems(removeCartItem(cartItems, cartItemToRemove));
     };
 
+    const clearItemFromCart = (cartItemToClear) => {
+      setCartItems(clearCartItem(cartItems, cartItemToClear));
+    };
 
     const value = {
          isCartOpen, 
          setIsCartOpen, 
          addItemToCart, 
          removeItemToCart,
+         clearItemFromCart,
          cartItems, 
          cartCount, 
     };
