3bfaa1c4890385944e3a8c811b2f96b5ab58bf11
YunusEmreUslu <usluyunusemre00@gmail.com>
2022-08-21 19:51:53 +0300

    added shop items info to firebase (128)

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,6 +15,8 @@ import {
     doc,
     getDoc,
     setDoc,
+    collection,
+    writeBatch,
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -41,6 +43,21 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey, objectsToAdd
+  ) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db); 
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+  
+  await batch.commit();
+  console.log('done');
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
